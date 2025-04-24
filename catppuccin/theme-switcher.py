929ca893d3333c12ae7ac97be38ec5d8d00adf58
taka <taka0o@users.noreply.github.com>
2025-04-24 09:16:25 -0400

    feat: upgrade to regolith 3 (#8)

--- catppuccin/theme-switcher.py
+++ catppuccin/theme-switcher.py
@@ -1,82 +1,72 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 import argparse
 import sys
-from subprocess import check_output as run
 
-flavours = ["mocha", "macchiato", "frappe", "latte"]
-possible_accents = [
-    "rosewater",
-    "flamingo",
-    "pink",
-    "mauve",
-    "red",
-    "maroon",
-    "peach",
-    "yellow",
-    "green",
-    "teal",
-    "sky",
-    "sapphire",
-    "blue",
-    "lavender"
+FLAVOURS = ["mocha", "macchiato", "frappe", "latte"]
+ACCENTS = [
+    "rosewater", "flamingo", "pink", "mauve", "red", "maroon",
+    "peach", "yellow", "green", "teal", "sky", "sapphire", "blue", "lavender"
 ]
-accents = [f"color_{accent}" for accent in possible_accents]
+ACCENT_KEYS = [f"color_{accent}" for accent in ACCENTS]
 
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    "--flavour",
-    help="Flavour of Catppuccin to use",
-    choices=flavours,
-    default="mocha"
-)
-parser.add_argument(
-    "--accent",
-    help="Accent to use",
-    choices=possible_accents,
-    default="blue"
-)
-args = parser.parse_args()
+ROOT_FILE_PATH = "/usr/share/regolith-look/catppuccin/root"
+CONFIG_DIR = "/usr/share/regolith-look/catppuccin/config"
 
-def main(argv):
-    FILE_PATH = "/usr/share/regolith-look/catppuccin/root"
-    with open(FILE_PATH, "r") as file:
+def parse_args():
+    parser = argparse.ArgumentParser(description="Apply Catppuccin flavour and accent.")
+    parser.add_argument(
+        "--flavour",
+        choices=FLAVOURS,
+        default="mocha",
+        help="Flavour of Catppuccin to use"
+    )
+    parser.add_argument(
+        "--accent",
+        choices=ACCENTS,
+        default="blue",
+        help="Accent to use"
+    )
+    return parser.parse_args()
+
+def update_root_file(flavour, accent):
+    with open(ROOT_FILE_PATH, "r") as file:
         lines = file.readlines()
 
-        for i, line in enumerate(lines):
-            for known_flavour in flavours:
-                if known_flavour in line:
-                    print(
-                        "Found {} in line {}, changing for {}".format(
-                            known_flavour, i, args.flavour
-                        )
-                    )
-                    lines[i] = line.replace(known_flavour, args.flavour)
+    for i, line in enumerate(lines):
+        for known_flavour in FLAVOURS:
+            if known_flavour in line:
+                print(f"Found {known_flavour} in line {i}, changing to {flavour}")
+                lines[i] = line.replace(known_flavour, flavour)
 
-            for known_accent in accents:
-                if known_accent in line:
-                    print(
-                        "Found {} in line {}, changing for {}".format(
-                            known_accent, i, args.accent
-                        )
-                    )
-                    lines[i] = line.replace(known_accent, f"color_{args.accent}")
+        for known_accent in ACCENT_KEYS:
+            if known_accent in line:
+                print(f"Found {known_accent} in line {i}, changing to color_{accent}")
+                lines[i] = line.replace(known_accent, f"color_{accent}")
 
-    with open(FILE_PATH, "w") as file:
+    with open(ROOT_FILE_PATH, "w") as file:
         file.writelines(lines)
 
-    FILE_PATH = f"/usr/share/regolith-look/catppuccin/config/{args.flavour}"
-    with open(FILE_PATH, "r") as file:
+def update_config_file(flavour):
+    config_path = f"{CONFIG_DIR}/{flavour}"
+    with open(config_path, "r") as file:
         lines = file.readlines()
-        for i, line in enumerate(lines):
-            if line.startswith("gtk.theme_name:"):
-                flavour = args.flavour.capitalize()
-                # TODO: replace this with {args.accent}, once GTK has all accents
-                lines[i] = f"gtk.theme_name: Catppuccin-{flavour}\n"
 
-    with open(FILE_PATH, "w") as file:
+    for i, line in enumerate(lines):
+        if line.startswith("gtk.theme_name:"):
+            theme_name = f"Catppuccin-{flavour.capitalize()}"
+            lines[i] = f"gtk.theme_name: {theme_name}\n"
+            # TODO: update to include accent when GTK themes support it
+            print(f"Updated GTK theme name to {theme_name}")
+
+    with open(config_path, "w") as file:
         file.writelines(lines)
 
+def main():
+    args = parse_args()
+    update_root_file(args.flavour, args.accent)
+    update_config_file(args.flavour)
 
 if __name__ == "__main__":
-    main(argv=sys.argv[1:])
+    main()
+
--- wrapper.sh
+++ wrapper.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-sudo python3 /usr/share/regolith-look/catppuccin/theme-switcher.py "${1:-mocha}" "${2:-blue}"
+sudo python3 /usr/share/regolith-look/catppuccin/theme-switcher.py --flavour "${1:-mocha}" --accent "${2:-blue}"
 regolith-look refresh
